c3d7d634d9d5bb8e216357c14b9271799dde6391
Marat Kanatov <kanatovmarat8@gmail.com>
2022-04-23 19:52:13 +0600

    Delete default code

---
+++ lib/my_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_like_in_video/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "My App",
+      home: HomeScreen(),
+    );
+  }
+}
